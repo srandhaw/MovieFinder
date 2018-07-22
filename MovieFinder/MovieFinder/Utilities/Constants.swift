a6ca5f063bef8ba625b12307ea8d1368833695d5
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-07-22 13:15:39 +0400

    rearranging of files

---
+++ MovieFinder/MovieFinder/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MovieFinder
+//
+//  Created by Sehajbir Randhawa on 7/22/18.
+//  Copyright © 2018 Sehajbir-Nikhil. All rights reserved.
+//
+
+import Foundation
+
+
+/*
+ Usage:
+ 
+ let API_KEY = 13eu23bii393210
+ */
