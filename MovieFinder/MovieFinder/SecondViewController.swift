2804932ccac8c5065f4c1d8db987bfe4ec4c93c7
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-07-22 12:22:07 +0400

    initial commit

---
+++ MovieFinder/MovieFinder/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MovieFinder
+//
+//  Created by Sehajbir Randhawa on 7/22/18.
+//  Copyright © 2018 Sehajbir-Nikhil. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
