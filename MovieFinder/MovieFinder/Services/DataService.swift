53608568a79b7d849a5588c94ebaa799e2052f86
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-07-22 14:31:21 +0400

    constants file update: Genre ids

---
+++ MovieFinder/MovieFinder/Services/DataService.swift
@@ -0,0 +1,23 @@
+//
+//  DataService.swift
+//  MovieFinder
+//
+//  Created by Sehajbir Randhawa on 7/22/18.
+//  Copyright © 2018 Sehajbir-Nikhil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class DataService{
+    
+    static let instance = DataService()
+    
+    var movieTitles: [String] = []
+    
+    
+    
+    
+}
